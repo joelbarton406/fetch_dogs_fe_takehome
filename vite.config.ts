4f996c4eb3599cbcdfb3208723b106e5a101ef72
joelbarton-io <joelbarton406@gmail.com>
2025-03-18 23:19:55 -0600

    event: switching to vercel deployment bc of browser router issues in gh pages.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/fetch_dogs_fe_takehome",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
